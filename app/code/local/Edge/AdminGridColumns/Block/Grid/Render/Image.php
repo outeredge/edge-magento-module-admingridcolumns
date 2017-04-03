9a5d40fbc964ad8d07327eafa11d439190c3b2b9
Bruno Dias <bruno@outeredgeuk.com>
2017-04-03 15:32:40 +0100

    Added try catch when image are not available

--- app/code/local/Edge/AdminGridColumns/Block/Grid/Render/Image.php
+++ app/code/local/Edge/AdminGridColumns/Block/Grid/Render/Image.php
@@ -5,7 +5,11 @@ class Edge_AdminGridColumns_Block_Grid_Render_Image extends Mage_Adminhtml_Block
     public function render(Varien_Object $row)
     {
         if ($row->getImage() && $row->getImage() !== 'no_selection') {
-            return '<img src="' . Mage::helper('edge/image')->setSize(100)->getImage('catalog/product' . $row->getImage()) . '" alt="">';
+            try {
+                return '<img src="' . Mage::helper('edge/image')->setSize(100)->getImage('catalog/product' . $row->getImage()) . '" alt="">';
+            } catch(exception $e) {
+                Mage::log($e->getMessage() . " - Rendering image for product ID: " . $row->getId(), null, 'admin_image.log');
+            }
         }
         return '';
     }
